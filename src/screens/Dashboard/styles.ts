f62f737c3cb98ed15724c2a4546fa8ebe773cf94
Hugo de Oliveira <hugo.lobo47@gmail.com>
2022-09-27 18:02:58 -0300

    introdução do loading e lista dinâmica

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -87,3 +87,9 @@ export const TransactionList = styled(
 })``;
 
 export const LogoutButton = styled(TouchableOpacity)``;
+
+export const LoadContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
