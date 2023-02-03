383b8248f6f0f61f96ca337e99ab047e8a128272
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-02-03 18:35:34 -0300

    retirada de lógica ternária p/ IOS

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -64,13 +64,12 @@ export function SignIn() {
               svg={GoogleSvg}
               onPress={handleSignInWithGoogle}
             />
-            {Platform.OS === "ios" && (
-              <SignInSocialButton
-                title="Entrar com Apple"
-                svg={AppleSvg}
-                onPress={handleSignInWithApple}
-              />
-            )}
+
+            <SignInSocialButton
+              title="Entrar com Apple"
+              svg={AppleSvg}
+              onPress={handleSignInWithApple}
+            />
           </FooterWrapper>
           {isLoading && (
             <ActivityIndicator
